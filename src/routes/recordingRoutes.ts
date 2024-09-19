8a7a53f9f7bf98db9cbb40f3cd15d5ccc6d6ea84
cd <lesleyrotonto@gmail.com>
2024-09-19 11:28:14 -0400

    protect routes

--- src/routes/recordingRoutes.ts
+++ src/routes/recordingRoutes.ts
@@ -1,12 +1,13 @@
 import Router from 'express';
 import { createNewRecording, getAllRecordings } from '../controllers/recordingController';
+import isLoggedIn from '../middleware/isLoggedIn';
 import upload from '../middleware/upload.js';
 
 const router = Router();
 
 router.get('/', getAllRecordings);
 
-router.post('/recordings', upload.fields([
+router.post('/recordings', isLoggedIn, upload.fields([
   { name: 'actorAudio'},
   { name: 'readerAudio'}
 ]), createNewRecording);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,12 +6,12 @@ const router = Router()
 
 router.get('/', isLoggedIn, getAllUsers)
 
-router.get('/:username', getUserByUsername)
+router.get('/:username', isLoggedIn, getUserByUsername)
 
 // router.post('/newUser', createUser)
 
-router.put('/:id', editUser)
+router.put('/:id', isLoggedIn, editUser)
 
-router.delete('/:id', deleteUser)
+router.delete('/:id', isLoggedIn, deleteUser)
 
 export default router
